a481385ec2ae2ef18aec13f47207171a74ece9fb
Gerardo3Carcamo <gcarcamo@uadec.edu.mx>
2026-01-11 23:01:26 -0600

    feat home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonModule, Button } from 'primeng/button'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [Button],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
